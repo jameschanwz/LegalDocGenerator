d8f63d2bf7e71d071b352074307b255f13e46870
jameschanwz <75669488+jameschanwz@users.noreply.github.com>
2021-03-15 14:02:26 +0800

    moving templates folder

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
 
-app = Flask(__Name__)
+app = Flask(__name__)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import render_template
 
-@route('/')
-@route('/index')
-
-return render_template('index.html',title = "Home")
+@app.route('/')
+@app.route('/index')
+def index():
+	return render_template('index.html',title = "Home")
