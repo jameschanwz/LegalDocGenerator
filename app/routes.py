103589f1ce4390faccd1de82ee52e88cc1e47456
jameschanwz <75669488+jameschanwz@users.noreply.github.com>
2021-03-17 15:52:19 +0800

    create nda page and form

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash
+from app.forms import NDAForm
 
 @app.route('/')
 @app.route('/index')
 def index():
 	return render_template('index.html',title = "Home")
+
+@app.route('/nda', methods = ['GET','POST'])
+def nda():
+	form = NDAForm()
+	if form.validate_on_submit():
+		flash ('NDA template created for {}'.format(form.partyname.data))
+	return render_template('NDA.html',title="NDA", form=form)
