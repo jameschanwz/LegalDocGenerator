7d0b77a7d2c2767c1605cd831eb2d674f1a62474
jameschanwz <75669488+jameschanwz@users.noreply.github.com>
2021-03-30 15:43:17 +0800

    changes in work com

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import Config
 
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config.from_object(Config)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template, flash
 from app.forms import NDAForm
+from docx import Document
 
 @app.route('/')
 @app.route('/index')
@@ -11,5 +12,10 @@ def index():
 def nda():
 	form = NDAForm()
 	if form.validate_on_submit():
+		document = Document()
+		paragraph = document.add_paragraph()
+		paragraph.add_run(text='Test para')
 		flash ('NDA template created for {}'.format(form.partyname.data))
+		document.save('testNDA.docx')
+		link = ('Your download is ready')
 	return render_template('NDA.html',title="NDA", form=form)
